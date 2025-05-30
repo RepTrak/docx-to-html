358882afd33135fdd7a81aee944ee58e42dadfa7
jliu2006 <jackliupenny@gmail.com>
2025-05-30 14:34:03 -0400

    added beautifulsoup script

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ Converting .docx to .html
 
 Exploring tools like BeautifulSoup or docx-parser-converter, may also attempt to convert from .docx to .pdf to .html, or .docx to .xml to .html.
 
-Running Python 3.10+
+Running Python 3.10.0
+
+The script using docx_parser_converter is currently blocked and unable to correctly convert from .docx to .html
+
+The script using Beautiful Soup is currently working and able to produce .html files. However, it is too lossy to be feasible (output .html file does not retain any text formatting or hierarchy). Will attempt to rewrite the script to make a smarter converter.
--- scripts/convert_docx.py
+++ scripts/convert_docx.py
@@ -1,20 +1,26 @@
-# need python 3.10+ to support the current version of docx-parser-converter, currently installing on local machine
-# earlier versions of python don't support the pipe operation for type unions:
-# def generate_html_body(doc_margins: DocMargins, elements: List[Paragraph | Table], numbering_schema: NumberingSchema) -> etree.Element:
-
 from docx_parser_converter.docx_to_html.docx_to_html_converter import DocxToHtmlConverter
 from docx_parser_converter.docx_to_txt.docx_to_txt_converter import DocxToTxtConverter
 from docx_parser_converter.docx_parsers.utils import read_binary_from_file_path
 from config import settings
+import os
 
-def convert_file(input, output):
+def docx_to_html(input, output):
     docx_file_content = read_binary_from_file_path(input)
-    converter = DocxToHtmlConverter(input, use_default_values=True)
+    converter = DocxToHtmlConverter(docx_file_content, use_default_values=True)
     html_output = converter.convert_to_html()
     converter.save_html_to_file(html_output, output)
+
+def docx_to_plain(input, output):
+    docx_file_content = read_binary_from_file_path(input)
+    converter = DocxToTxtConverter(docx_file_content, use_default_values=True)
+    txt_output = converter.convert_to_txt()
+    converter.save_txt_to_file(txt_output, output)
 
 for name in settings.docx_input_names:
     input_path = os.path.join(settings.docx_allstate_path, name)
     output_path = os.path.join(settings.html_output_folder, name.replace(".docx", ".html"))
     print(f"input = {input_path}, output = {output_path}")
-    convert_file(input_path, output_path)
+    docx_to_plain(input_path, output_path)
+
+    # currently blocked: when parsing the docx file, docx_parser_converter fails to correctly parse the .docx file's numbering.xml component.
+    # can try local patch to library or clean up .docx file
